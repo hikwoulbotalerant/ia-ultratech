7944040027a6f8938370ee83aa4c20ceb445c2ef
Hikson Talerant <166321299+hikwoulbotalerant@users.noreply.github.com>
2025-01-06 00:09:45 +0100

    Update page.tsx
    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
 import Image from "next/image";
+import { Mistral } from "@mistralai/mistralai";
+
+const apiKey = process.env.MISTRAL_API_KEY;
+const client = new Mistral({ apiKey: apiKey });
 
 export default function Home() {
+  async function handleChat() {
+    try {
+      const chatResponse = await client.chat.complete({
+        model: "mistral-large-latest",
+        messages: [
+          { role: "user", content: "What is the best French cheese?" },
+        ],
+      });
+      console.log("Chat:", chatResponse.choices[0].message.content);
+    } catch (error) {
+      console.error("Error fetching chat response:", error);
+    }
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -24,21 +42,12 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <button
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            onClick={handleChat}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
+            Fetch Chat Response
+          </button>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
